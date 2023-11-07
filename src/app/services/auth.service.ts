3df2e7fe6606e5383d76cfd2b2f116b9c272d516
candelariaSobol <candee_sobol@hotmail.com>
2023-11-07 14:17:30 -0300

    recibo el token y lo almaceno en el session storage

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,16 +1,35 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Login } from '../Models/login';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, map } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  url: string = 'http://localhost:3000/auth/login';
+  loggedIn = new BehaviorSubject<boolean>(false);
+  currentUserSubject: BehaviorSubject<Login> = new BehaviorSubject<Login>(
+    JSON.parse(localStorage.getItem('currentUser') || '{}')
+  );
+  currentUser: Observable<Login> = this.currentUserSubject.asObservable();
 
-  constructor(private http:HttpClient) { }
-
-  iniciarSesion(login: Login): Observable<any>{
-    return this.http.post('http://localhost:3000/auth/login', login)
+  constructor(private http: HttpClient) {
+    console.log('Servicio de Autenticación está corriendo');
   }
+
+  iniciarSesion(login: Login): Observable<any> {
+    return this.http.post(this.url, login).pipe(
+      map(data => {
+        const userData = data as Login; // Realiza una conversión explícita
+        sessionStorage.setItem('currentUser', JSON.stringify(userData));
+        this.currentUserSubject.next(userData);
+        this.loggedIn.next(true);
+        return userData;
+      })
+    );
+  }
+  
 }
+
